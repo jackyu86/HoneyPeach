b12c830065e13350d30cdedb91b23ab2e4aba807
jack yu <yuhaiyang@autohome.com.cn>
2017-12-11 18:12:29 +0800

    监控线程池代理与封装
    监控线程池代理与封装

---
+++ open-learn-template/open-learn-monitor-template/src/main/java/org/learn/open/monitor/model/ThreadPoolInformation.java
@@ -0,0 +1,69 @@
+package org.learn.open.monitor.model;
+
+/**
+ * @Author: jack-yu
+ * @Description:  线程池维度信息
+ */
+public class ThreadPoolInformation {
+
+    private String name;
+
+    private int threadNum;
+
+    private int coreThreadNum;
+
+    private int maxThreadNum;
+
+    private int queueLength;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getThreadNum() {
+        return threadNum;
+    }
+
+    public void setThreadNum(int threadNum) {
+        this.threadNum = threadNum;
+    }
+
+    public int getCoreThreadNum() {
+        return coreThreadNum;
+    }
+
+    public void setCoreThreadNum(int coreThreadNum) {
+        this.coreThreadNum = coreThreadNum;
+    }
+
+    public int getMaxThreadNum() {
+        return maxThreadNum;
+    }
+
+    public void setMaxThreadNum(int maxThreadNum) {
+        this.maxThreadNum = maxThreadNum;
+    }
+
+    public int getQueueLength() {
+        return queueLength;
+    }
+
+    public void setQueueLength(int queueLength) {
+        this.queueLength = queueLength;
+    }
+
+    @Override
+    public String toString() {
+        return "ThreadPoolInformation{" +
+                "name='" + name + '\'' +
+                ", threadNum=" + threadNum +
+                ", coreThreadNum=" + coreThreadNum +
+                ", maxThreadNum=" + maxThreadNum +
+                ", queueLength=" + queueLength +
+                '}';
+    }
+}
---
+++ open-learn-template/open-learn-monitor-template/src/main/java/org/learn/open/monitor/proxy/MonitorThreadPoolProxy.java
@@ -0,0 +1,78 @@
+package org.learn.open.monitor.proxy;
+
+
+import java.lang.reflect.Method;
+
+import org.learn.open.monitor.model.ThreadPoolInformation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+/**
+ * @Author: jack-yu
+ * @Description:  线程池代理创建获取维度信息
+ */
+public class MonitorThreadPoolProxy {
+
+    private static final Logger LOGGER= LoggerFactory.getLogger(MonitorThreadPoolProxy.class);
+
+    private Object threadPoolObject;
+
+    public MonitorThreadPoolProxy(Object threadPoolObject){
+        this.threadPoolObject=threadPoolObject;
+    }
+
+    public int getThreadNum(){
+        try{
+            Object threadNum=invokeFunc(threadPoolObject,"getPoolSize").invoke(threadPoolObject);
+            return (Integer)threadNum;
+        }catch (Exception e){
+            LOGGER.error("run getPoolSize() error.",e);
+            return -1;
+        }
+    }
+
+    public int getCoreThreadNum(){
+        try{
+            Object coreThreadNum=invokeFunc(threadPoolObject,"getCorePoolSize").invoke(threadPoolObject);
+            return (Integer)coreThreadNum;
+        }catch (Exception e){
+            LOGGER.error("run getCorePoolSize() error.",e);
+            return -1;
+        }
+    }
+
+    public int getMaxThreadNum(){
+        try{
+            Object maxThreadNum = invokeFunc(threadPoolObject,"getMaximumPoolSize").invoke(threadPoolObject);
+            return (Integer)maxThreadNum;
+        }catch (Exception e){
+            LOGGER.error("run getMaximumPoolSize() error.",e);
+            return -1;
+        }
+    }
+
+    public int getQueueLength(){
+        try{
+            Object queue=invokeFunc(threadPoolObject,"getQueue").invoke(threadPoolObject);
+            Object queueLength=invokeFunc(queue,"size").invoke(queue);
+            return (Integer)queueLength;
+        }catch (Exception e){
+            LOGGER.error("run getQueue() or queue.size() error.",e);
+            return -1;
+        }
+    }
+
+    public ThreadPoolInformation buildThreadPoolInformation(){
+        ThreadPoolInformation threadPoolInformation=new ThreadPoolInformation();
+        threadPoolInformation.setCoreThreadNum(getCoreThreadNum());
+        threadPoolInformation.setMaxThreadNum(getMaxThreadNum());
+        threadPoolInformation.setQueueLength(getQueueLength());
+        threadPoolInformation.setThreadNum(getThreadNum());
+        return threadPoolInformation;
+    }
+
+
+
+    private Method invokeFunc(Object obj,String funcName,Class<?>... parameterTypes) throws NoSuchMethodException{
+            return obj.getClass().getMethod(funcName,parameterTypes);
+    }
+}
